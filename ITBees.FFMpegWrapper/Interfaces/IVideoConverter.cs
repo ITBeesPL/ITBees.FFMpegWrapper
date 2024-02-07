0ce74afc1d83c3ba689d7fd59db0800785f5f10c
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-02-07 10:24:03 +0100

    Support for conversion from mp4 to mov and avi to mov

--- ITBees.FFMpegWrapper/Interfaces/IVideoConverter.cs
+++ ITBees.FFMpegWrapper/Interfaces/IVideoConverter.cs
@@ -6,4 +6,7 @@ public interface IVideoConverter
 {
     void ConvertWMVtoWebM(string inputPath, string outputPath,
         AudioBitrate audioBitrate = AudioBitrate.AudioBitrate128k, Crf crf = Crf.Crf20, Codec codec = Codec.vp9);
+    void ConvertWMVtoMov(string inputPath, string outputPath,
+        AudioBitrate audioBitrate = AudioBitrate.AudioBitrate128k, Crf crf = Crf.Crf20);
+    void ConvertMP4toMov(string inputPath, string outputPath);
 }
--- ITBees.FFMpegWrapper/VideoConverter.cs
+++ ITBees.FFMpegWrapper/VideoConverter.cs
@@ -24,4 +24,44 @@ public partial class VideoConverter : IVideoConverter
             process.WaitForExit();
         }
     }
+
+    public void ConvertWMVtoMov(string inputPath, string outputPath, AudioBitrate audioBitrate = AudioBitrate.AudioBitrate128k, Crf crf = Crf.Crf20)
+    {
+        var args =
+            $"-i \"{inputPath}\" -c:v libx264 -crf {((int)crf)} -preset slow -c:a aac -b:a {(int)audioBitrate}k \"{outputPath}\"";
+        var convertProcess = new ProcessStartInfo
+        {
+            FileName = "ffmpeg",
+            Arguments = args,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+        };
+
+        using (var process = new Process { StartInfo = convertProcess })
+        {
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+    public void ConvertMP4toMov(string inputPath, string outputPath)
+    {
+        var args = $"-i \"{inputPath}\" -c copy \"{outputPath}\"";
+
+        var convertProcess = new ProcessStartInfo
+        {
+            FileName = "ffmpeg",
+            Arguments = args,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+        };
+
+        using (var process = new Process { StartInfo = convertProcess })
+        {
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+
 }
