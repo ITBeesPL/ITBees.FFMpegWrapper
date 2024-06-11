601faeec124cf3661478e57282f567a2712f5ff5
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-06-11 23:03:40 +0200

    Update nuget references

---
+++ ITBees.FFMpegWrapper/VideoThumbnail.cs
@@ -0,0 +1,20 @@
+﻿using FFMediaToolkit;
+using FFmpeg.AutoGen;
+
+public class VideoThumbnailGenerator
+{
+    public static void GenerateThumbnail(string videoPath, string thumbnailPath, int width, int height)
+    {
+        FFmpegLoader.FFmpegPath = @"path_to_ffmpeg";
+        unsafe
+        {
+            AVFormatContext* formatContext = null;
+            if (ffmpeg.avformat_open_input(&formatContext, videoPath, null, null) < 0)
+            {
+                throw new ApplicationException("Opening video file has failed.");
+            }
+
+            // todo rest code method implementation
+        }
+    }
+}
